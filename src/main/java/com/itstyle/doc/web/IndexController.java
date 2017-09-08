d95f62a78d4023ca0893e710f7367f9fd8af20f5
小柒2012 <345849402@qq.com>
2017-09-08 13:18:15 +0800

    找回密码功能

--- src/main/java/com/itstyle/doc/web/IndexController.java
+++ src/main/java/com/itstyle/doc/web/IndexController.java
@@ -15,15 +15,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.google.code.kaptcha.impl.DefaultKaptcha;
 import com.itstyle.doc.common.constans.Constans;
 import com.itstyle.doc.common.constans.Option;
-import com.itstyle.doc.common.utils.MD5Util;
-import com.itstyle.doc.common.utils.Result;
 import com.itstyle.doc.model.Books;
-import com.itstyle.doc.model.Member;
 import com.itstyle.doc.repository.BooksRepository;
 import com.itstyle.doc.repository.MemberRepository;
 import com.itstyle.doc.repository.OptionsRepository;
@@ -48,39 +44,6 @@ public class IndexController {
 		 List<Books> bookList =  booksRepository.findAll();
 		 map.addAttribute("bookList",bookList);
 		 return "home/index";
-    }
-	@RequestMapping(value="login",method=RequestMethod.GET)
-    public String  login(ModelMap map) {
-		 logger.info("用户登陆 ");
-		 map.addAttribute("SITE_NAME", Constans.mapOptions.get(Option.SITE_NAME.getCode()));
-		 map.addAttribute("ENABLED_REGISTER", Constans.mapOptions.get(Option.ENABLED_REGISTER.getCode()));
-		 map.addAttribute("ENABLED_CAPTCHA", Constans.mapOptions.get(Option.ENABLED_CAPTCHA.getCode()));
-		 return "account/login";
-    }
-	@RequestMapping(value="login",method=RequestMethod.POST)
-    public @ResponseBody Result  login(Member member, String code,HttpServletRequest request) {
-		 Result result = new Result();
-		 logger.info("用户登陆 ");
-		 Member user = memberRepository.findByAccount(member.getAccount());
-		 String vrifyCode =  (String) request.getSession().getAttribute("vrifyCode");
-		 if(vrifyCode.equalsIgnoreCase(code)){
-			 if(user!=null){
-				 if(user.getPassword().equals(MD5Util.MD5(member.getPassword()))){
-					 request.getSession().setAttribute(Constans.CURRENT_USER, user);
-					 result.setCode(Constans.SUCCESS);
-				 }else{
-					 result.setCode(Constans.ERROR);
-					 result.setMsg("密码错误");
-				 }
-			 }else{
-				 result.setCode(Constans.ERROR);
-				 result.setMsg("账号不存在");
-			 }
-		 }else{
-			 result.setCode(Constans.ERROR);
-			 result.setMsg("验证码错误");
-		 }
-		 return result;
     }
 	@RequestMapping("/defaultKaptcha")  
     public void defaultKaptcha(HttpServletRequest httpServletRequest,HttpServletResponse httpServletResponse) throws Exception{  
---
+++ src/main/java/com/itstyle/doc/web/ManagerController.java
@@ -0,0 +1,16 @@
+package com.itstyle.doc.web;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import com.itstyle.doc.repository.MemberRepository;
+@Controller
+@RequestMapping(value = "")
+public class ManagerController {
+	private static final Logger logger = LoggerFactory.getLogger(ManagerController.class);
+	
+	@Autowired
+	MemberRepository memberRepository;
+	
+}
